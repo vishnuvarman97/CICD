2417cc3815af2b20f1e1b04753e011d4028ca8ec
vishnuvarman97 <148313919+vishnuvarman97@users.noreply.github.com>
2024-09-12 17:34:44 +0530

    1st commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_instance" "demo_server" {
+  ami           = "ami-0781c11a831c5f434"
+  instance_type = "t2.micro"
+  key_name       = "ubuntu"
+  
+  # Optionally specify an EBS volume
+  root_block_device {
+    volume_size = 8
+    volume_type = "gp2"
+  }
+  
+  tags = {
+    Name = "vish"
+  }
+}
