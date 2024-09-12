47ddbef61020fa5355c5dd451e78ce7167f9e037
vishnuvarman97 <148313919+vishnuvarman97@users.noreply.github.com>
2024-09-12 19:36:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,8 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_instance" "demo_server" {
-  ami           = "ami-0781c11a831c5f434"
+  ami           = "ami-0522ab6e1ddcc7055"
   instance_type = "t2.micro"
-  key_name       = "ubuntu"
   
   # Optionally specify an EBS volume
   root_block_device {
