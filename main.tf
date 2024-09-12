05d2572efda1bfb8e50076345533d2f991e9d1f0
vishnuvarman97 <148313919+vishnuvarman97@users.noreply.github.com>
2024-09-12 17:44:06 +0530

    main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
-provider "aws" {
-  region = "eu-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
 }
 
+provider "aws" {
+  # Configuration options
+}
 resource "aws_instance" "demo_server" {
   ami           = "ami-0781c11a831c5f434"
   instance_type = "t2.micro"
